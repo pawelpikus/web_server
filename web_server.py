8a3e3cfb698318337c55d7d72e6306b1d6acfaf6
Paweł Pikus <pawelpikus7@gmail.com>
2020-06-14 19:25:42 +0200

    Blog site using Flask

--- web_server.py
+++ web_server.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -13,3 +13,21 @@ def html_page(page_name):
     return render_template(page_name)
 
 
+@app.route('/submit_form', methods=['POST', 'GET'])
+def submit_form():
+    if request.method == 'POST':
+        name = request.form.get('name')
+        data = request.form
+        write_to_db(data)
+        return render_template('thank_you.html', value=name)
+    else:
+        return "Something went wrong. Try again!"
+
+
+def write_to_db(data):
+    with open('database.txt', 'a+') as db:
+        name = data['name']
+        email = data['email']
+        subject = data['subject']
+        message = data['message']
+        db.write(f"\n{name}, {email}: {subject}, {message}")
